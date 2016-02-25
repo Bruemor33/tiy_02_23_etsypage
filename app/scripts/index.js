3ce8b082b3e2bc41241511d27223772895fb846f
Bruemor <arymer1@gmail.com>
2016-02-24 22:40:14 -0500

    updates

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,45 @@
 console.log("Hello World!");
 
 var $ = require('jquery');
+var _ = require('underscore');
+var handlebars = require('handlebars');
+
+
+//w34fd7mc3m6lqlgrpjqz6yjs api key
+//wtyzcns6zf shared secret
+
+var url = "https://api.etsy.com/v2/listings/active.js?api_key=w34fd7mc3m6lqlgrpjqz6yjs&keywords=beer&includes=Images";
+
+function fetchJSONP(url, callback) {
+    var callbackName = 'jsonp_callback_' + Math.round(100000 * Math.random());
+    var script = document.createElement('script');
+
+    window[callbackName] = function(data) {
+        delete window[callbackName];
+        document.body.removeChild(script);
+        callback(data);
+    };
+
+    script.src = url + (url.indexOf('?') >= 0 ? '&' : '?') + 'callback=' + callbackName;
+    document.body.appendChild(script);
+
+}
+
+function logData(data){
+  console.log(data);
+  buildTemplates(data);
+}
+fetchJSONP(url, logData);
+
+
+
+function buildTemplates(data) {
+
+var resultsArray = data["results"];
+var source   = $("#top-catagories").html();
+var template = handlebars.compile(source);
+var html = template({resultsArray});
+
+$('#top-results').html(html);
+
+}
